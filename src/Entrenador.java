594604d5324079efbf9e7aa8d48789b8989e5782
liz15243 <lizethespi11@gmail.com>
2022-05-02 21:19:32 -0500

    Avance 3 Metodo tirar objeto pero el intercambiar no entendi muy bien el contexto

--- src/Entrenador.java
+++ src/Entrenador.java
@@ -1,4 +1,7 @@
-import java.util.ArrayList;
+import  java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Entrenador {
     // Region - String
     // PokemonMascota - Pokemon
@@ -59,14 +62,75 @@ public class Entrenador {
         Mochila = mochila;
     }
 
-    // Pelear - abstracto
-    //  Escoger pokemon de Pokedex
-    // Si su HP es superior a 15
-    // Escoger 2 fuertes y 1 debil aleatoriamente
-    // Si no tiene fuertes, poner 3 aleaotorios
-    //  Llamar al metodo pelear del pokemon
-    //
+    //pelear - Abstracto implementado
+    //escoger pokemon del pokedex
 
+    //si su HP es superior a 15
+    //escoger 2 fuertes y 1 debil aleatorios
+    //si no tiene fuertes, poner 3 aleaatorios
+    //arreglo de pokemones a usar en la pelea
+
+
+    //darle a escoger el pokemon al entrenador
+    //mostrar estadisticas del pokemon
+    //menu para pelear, curar, aumentar ataque o velocidad
+    //si escoge pelear
+    //llamar al metodo pelear del pokemon
+    //si escoge curar
+    //hay que validar si hay pocion/baya de curacion, usar
+    //si escoge +ataque
+    //hay que validar si hay pocion/baya de +ataque, usar
+    //si escoge velocidad
+    //hay que validar si hay pocion/baya de +velocidad, usar
+
+
+    //ganar metodo -> el entrenador opuesto ya no tienepokemones
+    //nivel 1 = 10 xp pokemon y entrenador
+    //nivel n = 10 xp * 1.5 pokemon y entrenador
+    //perder metodo -> yo me quedo sin pokemones
+
+    //TIRAR OBJETO
+    //mostrar los elementos de mochila
+   public void tirarObjeto(){
+       Scanner leer = new Scanner(System.in);
+      int indice = 1;
+      for(Objeto contenido : Mochila){
+          System.out.println( indice + " - ");
+          System.out.println(contenido);
+      }
+       //que el usuario escoja cual tirar
+       System.out.println(" Que deseas tirar");
+       int tirar= leer.nextInt();
+
+   }
+
+
+    //INTERCAMBIAR
+    public boolean intercambiar (int eleccion, boolean probabilidad){
+        //mostrar la pokedex
+        Scanner leer = new Scanner(System.in);
+        Random elegir = new Random();
+        int indice = 1;
+        for (Pokemon pokedex : Pokedex){
+            System.out.println( indice + " - " + pokedex);
+            indice++;
+        }
+
+        //mostrar la pokedex del entrenador opuesto
+        //escogo el pokemon que quiero
+        System.out.println("¿Que Pokemon deseas intercambiar");
+        eleccion = leer.nextInt();
+
+        //y con random true o false acepta
+        probabilidad = elegir.nextInt(100)>60;
+        //si true
+        if (probabilidad){
+
+        }
+        //get y add + remove
+
+        return false;
+    }
 
 
 
